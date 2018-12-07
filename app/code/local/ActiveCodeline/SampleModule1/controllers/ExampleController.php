d35036015e147866664041963ed3515428809b4a
Michele Capicchioni <capimichi@gmail.com>
2018-12-07 17:28:46 +0100

    Temp fix 4

--- app/code/local/ActiveCodeline/SampleModule1/controllers/ExampleController.php
+++ app/code/local/ActiveCodeline/SampleModule1/controllers/ExampleController.php
@@ -1,20 +1,20 @@
 <?php
 
-class Capimichi_ImportExport_Adminhtml_ExampleController extends Mage_Adminhtml_Controller_Action
+class ActiveCodeline_SampleModule1_ExampleController extends Mage_Adminhtml_Controller_Action
 {
 
     public function indexAction()
     {
-        // "Fetch" display
+    	// "Fetch" display
         $this->loadLayout();
         
         // "Inject" into display
         // THe below example will not actualy show anything since the core/template is empty
         $this->_addContent($this->getLayout()->createBlock('core/template'));
-
+        
         // echo "Hello developer...";
-
+                
         // "Output" display
         $this->renderLayout();
-    }
+    }	
 }
