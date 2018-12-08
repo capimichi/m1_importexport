106ccc5c4f68dcc8e15b9ead62aa7dddd527e5ed
Michele Capicchioni <capimichi@gmail.com>
2018-12-08 16:57:14 +0100

    Template

--- app/code/local/Capimichi/ImportExport/controllers/ImportController.php
+++ app/code/local/Capimichi/ImportExport/controllers/ImportController.php
@@ -10,7 +10,7 @@ class Capimichi_ImportExport_ImportController extends Mage_Adminhtml_Controller_
 
         // "Inject" into display
         // THe below example will not actualy show anything since the core/template is empty
-        $this->_addContent($this->getLayout()->createBlock('core/template')->setTemplate('capimichi/import_export/import/import.phtml')->toHtml());
+        $this->_addContent($this->getLayout()->createBlock('core/template')->setTemplate('capimichi/import_export/import/import.phtml'));
 
         // "Output" display
         $this->renderLayout();
