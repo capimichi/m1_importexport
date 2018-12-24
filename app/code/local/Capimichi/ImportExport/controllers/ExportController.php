dff79862dccc2b3a8dab71ca6bfb7c3806824fb4
Michele Capicchioni <capimichi@gmail.com>
2018-12-24 17:46:46 +0100

    Pages 17

--- app/code/local/Capimichi/ImportExport/controllers/ExportController.php
+++ app/code/local/Capimichi/ImportExport/controllers/ExportController.php
@@ -63,7 +63,9 @@ class Capimichi_ImportExport_ExportController extends Mage_Adminhtml_Controller_
                 if ($product->type_id == 'configurable') {
                     $childrenIds = Mage::getModel('catalog/product_type_configurable')
                         ->getChildrenIds($product->getId());
+                    $childrenIds = explode(",", $childrenIds);
                     foreach ($childrenIds as $childrenId) {
+                        $childrenId = trim($childrenId);
                         if (!in_array($childrenId, $exportedIds)) {
                             $childProduct = Mage::getModel('catalog/product')->load($childrenId);
                             $exportedIds[] = $childProduct->getId();
